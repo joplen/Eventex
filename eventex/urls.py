35bb928bb00b6807f1b1bd4037b4aeab8124b47c
guedesjoaoluis039@gmail.com <guedesjoaoluis039@gmail.com>
2013-07-25 09:29:08 -0300

    Aterao url static e index

--- eventex/urls.py
+++ eventex/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.conf import settings
+#from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
@@ -15,5 +15,5 @@ urlpatterns = patterns('eventex.core.views',
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
 )
-urlpatterns += patterns('',
-        url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),)
+#urlpatterns += patterns('',
+#        url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),)
