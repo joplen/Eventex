592825a6e8ae4ceae3d8754dec4adc5e40605131
guedesjoaoluis039@gmail.com <guedesjoaoluis039@gmail.com>
2013-07-25 08:23:59 -0300

    Atualizacao url static

---
+++ eventex/urls.py
@@ -0,0 +1,19 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('eventex.core.views',
+    url(r'^$', 'homepage', name='homepage'),
+    # url(r'^eventex/', include('eventex.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
+urlpatterns += patterns('',
+        url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),)
