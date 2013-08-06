44e329b45efd7283c7f037a1da718b6c42259b33
unknown <jmagalhaes@cnova_note167.casanova.local>
2013-08-06 09:23:32 -0300

    Separar DDD de phone.

--- eventex/subscriptions/tests/test_forms.py
+++ eventex/subscriptions/tests/test_forms.py
@@ -23,10 +23,21 @@ class SubscriptionFormTest(TestCase):
         form = self.make_validated_form(email='')
         self.assertFalse(form.errors)
 
+    def test_name_must_be_capitalized(self):
+        'Name must be capitalized.'
+        form = self.make_validated_form(name=u'JOÃO guedes')
+        self.assertEqual(u'João Guedes', form.cleaned_data['name'])
+
+    def test_must_inform_email_or_phone(self):
+        'Email and Phone are optional, but one must be informed.'
+        form = self.make_validated_form(email='',phone_0='', phone_1='')
+        self.assertItemsEqual(['__all__'], form.errors)
+        
+
 
     def make_validated_form(self, **kwargs):
         data = dict(name='Joao Guedes', email='joao@guedes.eu',
-                    cpf='12345678901', phone='15-12121212')
+                    cpf='12345678901', phone_0='15', phone_1='12121212')
         data.update(kwargs)
         form = SubscriptionForm(data)
         form.is_valid()
