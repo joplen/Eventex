e81083db4705081dfb8baa131f2973c62dd4ac3a
unknown <jmagalhaes@cnova_note167.casanova.local>
2013-08-06 14:17:56 -0300

    sem alterao

--- eventex/subscriptions/models.py
+++ eventex/subscriptions/models.py
@@ -2,6 +2,7 @@
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
+#
 class Subscription(models.Model):
     name = models.CharField(_('nome'), max_length=100)
     cpf = models.CharField(_('CPF'), max_length=11, unique=True)
@@ -9,11 +10,11 @@ class Subscription(models.Model):
     phone = models.CharField(_('telefone'), max_length=20, blank=True)
     created_at = models.DateTimeField(_('criado em'), auto_now_add=True)
     paid = models.BooleanField(_('Pago'))
-
+#
     class Meta:
         ordering = ['created_at']
         verbose_name = _(u'inscrição')
         verbose_name_plural = _(u'inscrições')
-
+#
     def __unicode__(self):
         return self.name
